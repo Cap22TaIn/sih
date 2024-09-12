63bd6c2e649b164b5b32327ee419eb11603a1ff8
CapTaIN <maxmax.mishra@gmail.com>
2024-09-12 17:35:20 +0530

    "html updated"

--- server.js
+++ server.js
@@ -7,19 +7,16 @@ const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public'))); // Serves static files like CSS, JS, images
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html')); // Correct path to index.html
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.post('/submit', (req, res) => {
     const formData = req.body;
-
-    // Format the data for CSV
     const dataRow = Object.values(formData).join(',') + '\n';
 
-    // Append the data to the CSV file
     fs.appendFile('data_points.csv', dataRow, (err) => {
         if (err) {
             console.error('Error writing to CSV file', err);
